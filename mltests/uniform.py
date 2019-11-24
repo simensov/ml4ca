0fe3e6738a8a2e3e2ba7f25a62031ce05df2b5b6
Simen Oevereng <simen.sem.overeng@dnvgl.com>
2019-11-24 17:34:16 +0100

    Add an example of how to distribute the dataset better

---
+++ mltests/uniform.py
@@ -0,0 +1,59 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+import time
+
+np.random.seed(int(time.time()))
+x = []
+
+a = np.linspace(-100,100,201)
+b = np.linspace(-100,100,201)
+
+ll = 1000
+for _ in range(ll):
+	i = np.random.choice(a)
+	j = np.random.choice(b)
+	x.append(i*j)
+
+
+def chunks(l, n):
+    n = max(1, n)
+    return [l[i:i+n] for i in range(0, len(l), n)]
+
+
+x.sort()
+x = np.array(x)
+
+dx = 500
+ranges = []
+
+# Increase occurences of few representations, and decrease occurences in many occurences
+# Allow maximum five representations of elements within each 100th sector
+for el in range(int(np.min(x)),int(np.max(x)),dx):
+
+	# Find where x has elements within current range
+	y = x[np.where((el < x) & (x < (el + dx)))]
+
+	# If there was no occurences, there is nothing to add to the set: move on
+	if y.shape[0] == 0:
+		continue
+
+	# If there are less than five occurences of datapoints in this range, add random datapoints from the set into the set
+	# Store old array in order to avoid increasing the probability of the first chosen element getting picked several times
+	ytemp = np.copy(y)
+	while y.shape[0] < 5:
+		y = np.hstack((y,np.random.choice(ytemp)))
+
+	# If there exists more than five elements in this range, remove random elements until there are five left
+	while y.shape[0] > 5:
+		y = np.delete(y,np.random.randint(0,len(y)),0)
+
+	# Extend the vector containing all datapoints
+	for j in y:
+		ranges.append(j)
+
+plt.figure()
+plt.subplot(211)
+plt.hist(x,bins = int(ll / 100))
+plt.subplot(212)
+plt.hist(ranges,bins=len(ranges))
+plt.show()

eb8f0a6f9507f7a8676e45cc662ec8a4f1864c93
simensov <simensem@gmail.com>
2020-03-17 23:43:44 +0100

    Improve agent code in terms of computational efficiency, and shapes to work with Keras

--- src/rl/windows_workspace/agents/ppo.py
+++ src/rl/windows_workspace/agents/ppo.py
@@ -55,7 +55,7 @@ class PPO(object):
         self.dummy_val, self.dummy_act = np.zeros((1,1)), np.zeros((1,num_actions))
 
     def build_critic(self):
-        inn = Input(shape = (self.num_states,))
+        inn = Input(shape = (self.num_states,),name='critic_input')
         x = Dense(self.layer_dims[0], activation = 'tanh')(inn)
         for i, hidden_nodes in enumerate(self.layer_dims):
             if i == 0: continue
@@ -63,57 +63,66 @@ class PPO(object):
         x = Dense(1)(x)
         model = Model(inputs = [inn], outputs=[x]) # TODO replace with sequential from FFNN?
         model.compile(optimizer = Adam(lr = self.lr_c), loss = 'mse')
+        # model.summary()
         return model
 
     def V(self,state):
-        return self.critic(state)
+        return self.critic.predict([state.reshape(1,self.num_states)]) # TODO a normal forward pass could be beneficial here!
 
-    def clip_loss(self,A,pred_t):
+    def clip_loss_fn(self,A,pred_t):
         ''' Returns a loss function used when compiling actor network. Compared to the paper, which maximizes the objective, we minimize the loss, hence the minus in the end'''
         # TODO there should be two more penalities here: exploration/entropy (probably not needed due to gaussian noise) and value function
+        # -> total_loss = critic_coeff * K.mean(K.square(rewards - values)) + actor_loss - entropy_beta * K.mean(-(newpolicy_probs * K.log(newpolicy_probs + 1e-10)))
         def loss(true_tp1, pred_tp1):
             variance = K.square(self.actor_noise)
             new_prob = normal_dist(pred_tp1,true_tp1,variance)
             old_prob = normal_dist(pred_t,true_tp1,variance)
-            ratio = new_prob / (old_prob + np.random.uniform(-1,1)*(1e-10)) # 
+            # TODO represent these as logs? Seems better for computational complexity
+            ratio = K.exp( K.log(new_prob + 1e-10) - K.log(old_prob + 1e-10) )
+            # ratio = new_prob / (old_prob + 1e-10) 
             clipval = K.clip(ratio,1-self.actor_epsilon, 1+self.actor_epsilon) * A
-            return -K.mean(K.minimum(ratio * A, clipval))
+            return -K.mean(K.minimum(ratio * A, clipval)) # maximize surrogate: minimize negative surrogate
 
         return loss
 
     def build_actor(self):
-        inn = Input(shape = (self.num_states,))
-        A = Input(shape = (1,)) # advantage
-        old_pred = Input(shape=(self.num_actions,))
+        inn = Input(shape = (self.num_states,),name='actor_input')
+        A = Input(shape = (1,),name='actor_adv') # advantage
+        old_pred = Input(shape=(self.num_actions,),name='actor_oldpred')
         x = Dense(self.layer_dims[0], activation = 'tanh')(inn)
         for i, hidden_nodes in enumerate(self.layer_dims):
             if i == 0: continue 
-            x = Dense(hidden_nodes, activation = 'tanh')(x)
-        x = Dense(self.num_actions, activation='tanh')(x)
+            x = Dense(hidden_nodes, kernel_initializer = 'glorot_uniform', bias_initializer = 'normal', activation = 'tanh')(x)
+        x = Dense(self.num_actions, kernel_initializer = 'glorot_uniform', bias_initializer = 'normal', activation = 'tanh')(x)
         model = Model(inputs=[inn, A, old_pred], outputs = [x])
-        model.compile(optimizer=Adam(lr=self.actor_lr), loss = self.clip_loss(A,old_pred))
-        model.summary()
+        model.compile(optimizer=Adam(lr=self.actor_lr), loss = self.clip_loss_fn(A,old_pred))
+        # model.summary()
         return model
 
     def act(self,obs):
-        prob = self.actor([obs.reshape(1,self.num_states), self.dummmy_val, self.dummy_act]) # forward pass TODO predict used earlier
+        pred = self.actor.predict([obs.reshape(1,self.num_states), self.dummy_val, self.dummy_act]) # forward pass TODO predict used earlier        
+        
         # TODO seems like a eps greedy thing here
         if np.random.random() < 0.1:
-            act = act_matrix = prob[0] + np.random.normal(loc=0, scale=self.actor_noise, size=prob[0].shape)
+            act = pred + np.random.normal(loc=0, scale=self.actor_noise, size=pred[0].shape) # before it said pred[0]
         else:
-            act = act_matrix = prob[0]
+            act = pred
 
-        return act, act_matrix, prob
+        return act.reshape((self.num_actions,)), pred.reshape((self.num_actions,))
 
     # TODO collection of batch happens outside, see get_batch() from https://github.com/LuEE-C/PPO-Keras/blob/master/Main.py 
     def update(self, batch):
+        ''' o: observations - a: actions under new policy - p: predicted actions from old policy - r: discounted rewards (r + gamma * V) '''
         o, a, p, r = batch
         o, a, p, r = o[:BATCH_SIZE], a[:BATCH_SIZE], p[:BATCH_SIZE], r[:BATCH_SIZE]
         old_prediction = p
-        pred_values    = self.critic(o) # self.critic.predict(o) # forward pass
+        pred_values    = self.critic.predict(o) # self.critic.predict(o) # forward pass
         advantage      = r - pred_values
+        print(np.mean(advantage))
         actor_loss     = self.actor.fit([o, advantage, old_prediction], [a], batch_size=BATCH_SIZE, shuffle=True, epochs=EPOCHS, verbose=False)
         critic_loss    = self.critic.fit([o], [r], batch_size=BATCH_SIZE, shuffle=True, epochs=EPOCHS, verbose=False)
+
+        return advantage, actor_loss, critic_loss
 
     def load(self,filename='model.h5'):
         pass # TODO see src/sl/SupervisedTau.py
@@ -121,33 +130,38 @@ class PPO(object):
     def save(self,filename='model.h5'):
         pass # TODO see src/sl/SupervisedTau.py
 
-'''
 
-# alternative loss, including entropy and value function loss!
-def ppo_loss(oldpolicy_probs, advantages, rewards, values):
-    def loss(y_true, y_pred):
-        newpolicy_probs = y_pred
-        ratio = K.exp(K.log(newpolicy_probs + 1e-10) - K.log(oldpolicy_probs + 1e-10))
-        p1 = ratio * advantages
-        p2 = K.clip(ratio, min_value=1 - clipping_val, max_value=1 + clipping_val) * advantages
-        actor_loss = -K.mean(K.minimum(p1, p2))
-        critic_loss = K.mean(K.square(rewards - values))
-        total_loss = critic_discount * critic_loss + actor_loss - entropy_beta * K.mean(
-            -(newpolicy_probs * K.log(newpolicy_probs + 1e-10)))
-        return total_loss
+if __name__ == '__main__':
+    agent = PPO()
 
-    return loss
+        
+    '''
 
-# automate advantage calculations!
-def get_advantages(values, masks, rewards):
-    returns = []
-    gae = 0
-    for i in reversed(range(len(rewards))):
-        delta = rewards[i] + gamma * values[i + 1] * masks[i] - values[i]
-        gae = delta + gamma * lmbda * masks[i] * gae
-        returns.insert(0, gae + values[i])
+    # alternative loss, including entropy and value function loss!
+    def ppo_loss(oldpolicy_probs, advantages, rewards, values):
+        def loss(y_true, y_pred):
+            newpolicy_probs = y_pred
+            ratio = K.exp(K.log(newpolicy_probs + 1e-10) - K.log(oldpolicy_probs + 1e-10))
+            p1 = ratio * advantages
+            p2 = K.clip(ratio, min_value=1 - clipping_val, max_value=1 + clipping_val) * advantages
+            actor_loss = -K.mean(K.minimum(p1, p2))
+            critic_loss = K.mean(K.square(rewards - values))
+            total_loss = critic_discount * critic_loss + actor_loss - entropy_beta * K.mean(
+                -(newpolicy_probs * K.log(newpolicy_probs + 1e-10)))
+            return total_loss
 
-    adv = np.array(returns) - values[:-1]
-    return returns, (adv - np.mean(adv)) / (np.std(adv) + 1e-10)
+        return loss
 
-'''
+    # automate advantage calculations!
+    def get_advantages(values, masks, rewards):
+        returns = []
+        gae = 0
+        for i in reversed(range(len(rewards))):
+            delta = rewards[i] + gamma * values[i + 1] * masks[i] - values[i]
+            gae = delta + gamma * lmbda * masks[i] * gae
+            returns.insert(0, gae + values[i])
+
+        adv = np.array(returns) - values[:-1]
+        return returns, (adv - np.mean(adv)) / (np.std(adv) + 1e-10)
+
+    '''

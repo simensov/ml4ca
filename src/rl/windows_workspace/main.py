a983cd802c778e47dce00a95ed88dbac796f9cf5
simensov <simensem@gmail.com>
2020-04-02 17:34:04 +0200

    Clean main and move necessary functions into trainer after testing that it works with the simulator

--- src/rl/windows_workspace/main.py
+++ src/rl/windows_workspace/main.py
@@ -1,197 +1,22 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Mar  6 11:54:44 2020
-
-@author: JONOM
-"""
-
-from digitwin import DigiTwin
-import threading
-from utils.log import log, forcelog
-import math as m
-import random as r
-import time
-
-from errorFrame import ErrorFrame 
-# from utils.mathematics import *
-from utils.simtools import get_pose_3DOF, get_vel_3DOF, get_random_pose, get_pose_on_radius
-from utils.debug import print_pose
-from environment import FixedThrusters
-from customEnv import RevoltSimple, Revolt
-from agents.ppo import PPO
-import numpy as np
-import matplotlib.pyplot as plt 
-import multiprocessing # TODO
-from textwrap import dedent
-
-#defs
-SIM_CONFIG_PATH     = "C:\\Users\\simen\\Documents\\Utdanning\\GTK\\configuration"
-SIM_PATH            = "C:\\Users\\simen\\Documents\\Utdanning\\GTK\\revoltsim\\bin\\revoltsim64.exe"
-PYTHON_PORT_INITIAL = 25338
-LOAD_SIM_CFG        = False
-NON_SIM_DEBUG       = False
-REPORT              = True #write out what is written to sim
-REPORTRESETS        = False
-NUM_SIMULATORS      = 1
-THREADING           = False # = True if threading out sim process. TODO trouble during venv - not tried using sys
-NUM_EPISODES        = 60 # One sim, 300 episodes, 5000 steps ~ 12 hours with 100 Hz
-OLD_CODE            = False
-
-
-def reset_sim(sim,**init):
-    #set init values
-    for modfeat in init:
-        module, feature = modfeat.split('.')
-        sim.val(module, feature, init[modfeat])
-        
-    #reset critical models to clear states from last episode
-    sim.val('Hull', 'StateResetOn', 1, REPORTRESETS)
-    sim.val('THR1', 'LinActuator', int(2), REPORTRESETS)
-    sim.step(50) #min 50 steps should do it
-    sim.val('Hull', 'StateResetOn', 0, REPORTRESETS)
-
-    sim.val('THR1', 'MtcOn', 1, REPORTRESETS) # bow
-    sim.val('THR1', 'AzmCmdMtc', 0.5*m.pi, REPORTRESETS)
-    sim.val('THR1', 'ThrustOrTorqueCmdMtc', 0.0, REPORTRESETS) 
-    sim.step(50) #min 50 steps should do it
-    sim.val('THR2', 'MtcOn', 1, REPORTRESETS) # stern, portside
-    sim.val('THR2', 'ThrustOrTorqueCmdMtc', -30, REPORTRESETS) 
-    sim.val('THR2', 'AzmCmdMtc', 0*m.pi, REPORTRESETS)
-    sim.val('THR3', 'MtcOn', 1, REPORTRESETS) # stern, starboard
-    sim.val('THR3', 'ThrustOrTorqueCmdMtc', 30, REPORTRESETS) 
-    sim.val('THR3', 'AzmCmdMtc', 0*m.pi, REPORTRESETS)
-    return
-
 '''
-Relevant parameters and outputs:
-    PARAMS: Hull.posAttitude [0,1,2] roll, pitch, yaw I guess
-    OUTPUT: Hull.Eta [0,1,2,3,4,5] 
-    OUTPUT: Hull.Nu [0,1,2,3,4,5] body velocity
-    OUTPUT: Hull.SurgeSpeed and .SwaySpeed - compare to nu-vector?
-    OUTPUT: Hull.Yaw 
+
+@author Simen Sem Oevereng, simensem@gmail.com
 '''
-    
-def simulate_episode(sim, **init):
-    '''
-    NB: adding / removing arguments in thus function might make it hard for the threading 
-    '''
-    reset_sim(sim,**init)
-    steps = 4000 # TODO upper limit for PPO
-    err = ErrorFrame(pos=get_pose_3DOF(sim)) # TODO passing reference point to this function?
-    p_body = err.get_pose()
 
-    for step in range(steps):
-        # TODO RL: Here, the action choice has to be done
-        sim.step() # Step the simulation 1 step or X steps digitwin.step(x)
-        # Observe new state and reward. 
-        pose = get_pose_3DOF(sim)
-        p_body = err.get_pose(pose)
-        vel = get_vel_3DOF(sim)
-        print_pose(p_body,'Err') if step % 500 == 0 else None
+from trainer import Trainer
 
-def episode_test(env,**init):
-    ''' Function to move into the trainer class '''
-    batch_size = 256 # factors of 256. trainer - training during episodes: n-step TD instead of 1-step or Monte Carlo
-    episode_length = batch_size * 4 # trainer
-    gamma = 0.99 # trainer
-    # print("episode by ", env.sim.name)
-
-    agent = PPO(num_states=env.num_states,num_actions=env.num_actions)
-    s = env.reset(**init)
-    states, actions, pred_actions, rewards = [], [], [], [] 
-    episodal_reward = 0
-
-    for t in range(episode_length):
-        a_pi, a_old_pi = agent.act(s)
-        sn, r, done, info = env.step(a_pi) # TODO done needs to be used so that batches don't contain random states next to each other
-        states.append(s) # TODO standardize state
-        actions.append(a_pi)
-        pred_actions.append(a_old_pi)
-        rewards.append(r)
-        s = sn
-        episodal_reward += r
-
-        if (t+1) % batch_size == 0 or t == episode_length-1: # update if batch size or if time steps has reached limit (important as DP operations dont have a terminal state)
-            
-            v_sn = agent.V(sn) # this is V(sT) from eq. 10 in the paper: the last state of the trajectory
-            discounted_rewards = []
-            for reward in rewards[::-1]: # reverse the list
-                v_sn = reward + gamma * v_sn
-                discounted_rewards.append(v_sn) # advantages are calculated by the agent, using its V-predictions
-
-            discounted_rewards.reverse() 
-            # TODO standardize returns: 
-            # https://medium.com/@thechrisyoon/deriving-policy-gradients-and-implementing-reinforce-f887949bd63
-            # -> https://arxiv.org/abs/1506.02438
-
-
-            batch = np.vstack(states), np.vstack(actions), np.vstack(pred_actions), np.vstack(discounted_rewards)
-            states, actions, pred_actions, rewards = [], [], [], []
-            advantages, actor_loss, critic_loss = agent.update(batch)
-    
-    # print(episodal_reward)
-    return episodal_reward
-        
-'''
-MAIN
-'''
 
 if __name__ == "__main__":
-
-    # Vars
-    sims = []
-    sim_semaphores = []
     
-    #Start up all simulators
-    for sim_ix in range(NUM_SIMULATORS):
-        python_port = PYTHON_PORT_INITIAL + sim_ix
-        log("Open CS sim " + str(sim_ix) + " Python_port=" + str(python_port))
-        sims.append(None)
-        if not NON_SIM_DEBUG:
-            sims[-1] = DigiTwin('Sim'+str(1+sim_ix), LOAD_SIM_CFG, SIM_PATH, SIM_CONFIG_PATH, python_port)
-        sim_semaphores.append(threading.Semaphore())
-        
-    log("Connected to simulators and configuration loaded")
+    #defs
+    SIM_CONFIG_PATH     = "C:\\Users\\simen\\Documents\\Utdanning\\GTK\\configuration"
+    SIM_PATH            = "C:\\Users\\simen\\Documents\\Utdanning\\GTK\\revoltsim\\bin\\revoltsim64.exe"
+    PYTHON_PORT_INITIAL = 25338
+    LOAD_SIM_CFG        = False
+    NUM_SIMULATORS      = 1
+    NUM_EPISODES        = 1000 # One sim, 300 episodes, 5000 steps ~ 12 hours with 100 Hz
 
-    # envs = [FixedThrusters(s) for s in sims]
-    envs = [Revolt(s) for s in sims]
-    all_rewards = [[]*NUM_SIMULATORS]
-
-    for ep_ix in range(NUM_EPISODES):
-        for sim_ix in range(NUM_SIMULATORS):
-            N, E, Y = get_pose_on_radius() # get_random_pose()
-            init = {'Hull.PosNED':[N,E],'Hull.PosAttitude':[0,0,Y]}
-
-            if THREADING:
-                sim_semaphores[sim_ix].acquire()
-                log("Locking sim" + str(sim_ix+1) + "/" + str(NUM_SIMULATORS))
-                # t = threading.Thread(target=simulate_episode, args=[sims[sim_ix]], kwargs=init)
-                t = threading.Thread(target=episode_test, args=[envs[sim_ix]], kwargs=init)
-                t.daemon = True
-                t.start()
-
-                # TODO hvis alle threads kunne ha delt en felles trajectory buffer hadde det vært awsome
-    
-            else:
-                if OLD_CODE:
-                    simulate_episode(sims[sim_ix],**init)
-                else:
-                    print(dedent('''\
-                        Ep: {}
-                        Sim: {}
-                    '''.format(ep_ix,sim_ix) ))
-                    
-                    episode_reward = episode_test(envs[sim_ix],**init)
-                    all_rewards[sim_ix].append(episode_reward)
-
-    if not THREADING and not OLD_CODE:
-        for rewards in all_rewards:
-            print(rewards)
-            plt.figure()
-            plt.plot([i for i in range(NUM_EPISODES)], rewards)
-            plt.xlabel('Episodes')
-            plt.ylabel('Episodal rewards')
-        plt.show()
-
-    # TODO display performance from stored model
-    
+    trainer = Trainer(NUM_SIMULATORS)
+    trainer.start_simulators(SIM_PATH,PYTHON_PORT_INITIAL,SIM_CONFIG_PATH,LOAD_SIM_CFG)
+    trainer.train(n_episodes = NUM_EPISODES)
--- src/rl/windows_workspace/trainer.py
+++ src/rl/windows_workspace/trainer.py
@@ -1,68 +1,114 @@
 import numpy as np
-from environment import RevoltSimulator
+from digitwin import DigiTwin
+import time
+from misc.simtools import get_pose_on_radius, standardize_state
+from customEnv import RevoltSimple
+from agents.ppo import PPO
+import matplotlib.pyplot as plt
+import datetime
 
 class Trainer(object):
 
     def __init__(self,
-                 env,
-                 agent,
-                 episodes,
-                 batch,
-                 gamma):
+                 n_sims):
         
-        self.envs = [] # envs # list of independent environments
-        self.agent = agent
-        self.episodes = episodes
-        self.batch_size = batch_size
-        self.gamma = gamma
+        assert isinstance(n_sims,int) and n_sims > 0, 'Number of simulators must be an integer'
+        self._n_sims     = n_sims
+        self._digitwins  = []*n_sims  # list of independent simulators
+        self._gamma      = 0.99
+        self._batch_size = 256
+        self._ep_len = 4 * self._batch_size 
 
-    def start_simulators(self):
-        sims = []
-        sim_semaphores = []
-        NUM_SIMULATORS = len(self.envs)
-
+    def start_simulators(self,sim_path,python_port_initial,sim_cfg_path,load_cfg):
         #Start up all simulators
-        for sim_ix in range(NUM_SIMULATORS):
-            python_port = PYTHON_PORT_INITIAL + sim_ix
-            log("Open CS sim " + str(sim_ix) + " Python_port=" + str(python_port))
-            sims.append(None)
-            if not NON_SIM_DEBUG:
-                sims[-1] = DigiTwin('Sim'+str(1+sim_ix), LOAD_SIM_CFG, SIM_PATH, SIM_CONFIG_PATH, python_port)
-            sim_semaphores.append(threading.Semaphore())
+        for sim_ix in range(self._n_sims):
+            python_port = python_port_initial + sim_ix
+            print("Open CS sim " + str(sim_ix) + " Python_port=" + str(python_port))
+            self._digitwins.append(None)
+            self._digitwins[-1] = DigiTwin('Sim'+str(1+sim_ix), load_cfg, sim_path, sim_cfg_path, python_port)
+        print("Connected to simulators and configuration loaded")
 
-            self.envs.append(RevoltSimulator(sims[-1]))
+    def get_simulators(self):
+        return self._digitwins      
 
-    def episode(self,episode_length : int = 5000):
-        s = self.env.reset()
-        states, actions, rewards = [], [], [] # todo could be stores as tuples
+    def run_episode(self,env,agent,**init):
+
+        s = env.reset(**init)
+        states, actions, pred_actions, rewards = [], [], [], []
         episodal_reward = 0
-        for t in range(episode_length):
-            # env.render() happens automatically?
-            a = self.agent.act(s)
-            sn, r, done, info = self.env.step(a)
-            states.append(s)
-            actions.append(a)
-            rewards.append(r)
 
+        for t in range(self._ep_len):
+            s = standardize_state(s)
+            a_pi, a_old_pi = agent.act(s)
+            sn, r, done, info = env.step(a_pi) # TODO done needs to be used so that batches don't contain random states next to each other
+            states.append(s) 
+            actions.append(a_pi)
+            pred_actions.append(a_old_pi)
+            rewards.append(r)
             s = sn
             episodal_reward += r
 
-            if (t+1) % self.batch_size == 0 or t == episode_length-1: # update if batch size or if time steps has reached limit (important as DP operations dont have a terminal state)
-                v_sn = self.agent.V(sn)
-
+            if (t+1) % self._batch_size == 0 or t == self._ep_len-1: # update if batch size or if time steps has reached limit (important as DP operations dont have a terminal state)
+                
+                sn = standardize_state(sn)
+                v_sn = agent.V(sn) # this is V(sT) from eq. 10 in the paper: the last state of the trajectory
                 discounted_rewards = []
                 for reward in rewards[::-1]: # reverse the list
-                    v_sn = reward + self.gamma * v_sn
-                    discounted_rewards.append(v_sn)
+                    v_sn = reward + self._gamma * v_sn
+                    discounted_rewards.append(v_sn) # advantages are calculated by the agent, using its V-predictions
 
-                discounted_rewards.reverse()
-                states_batch, actions_batch, reward_batch = np.vstack(states), np.vstack(actions), np.array(discounted_rewards)[:, np.newaxis]
-                states, actions, rewards = [], [], []
-                self.agent.update(states_batch, actions_batch, reward_batch) # Entranar el Cliente y el actor (Estado, acciones, discounted_r)
-        return
-    
-    def train(self):
+                discounted_rewards.reverse() 
+                # TODO standardize returns: 
+                # https://medium.com/@thechrisyoon/deriving-policy-gradients-and-implementing-reinforce-f887949bd63
+                # -> https://arxiv.org/abs/1506.02438
 
-        for _ in range(self.episodes):
-            self.episode()
+                batch = np.vstack(states), np.vstack(actions), np.vstack(pred_actions), np.vstack(discounted_rewards)
+                states, actions, pred_actions, rewards = [], [], [], []
+                advantages, actor_loss, critic_loss = agent.update(batch)
+        
+        return episodal_reward
 
+    def train(self,n_episodes = 1000):
+
+        # Setup envs and agents for training
+        envs = [RevoltSimple(s) for s in self._digitwins]
+        agents = [PPO(num_states=env.num_states,num_actions=env.num_actions) for env in envs]
+        all_rewards = [[]*self._n_sims]
+
+        time_begin = time.time()
+        for ep_ix in range(n_episodes):
+            for sim_ix in range(self._n_sims):
+                N, E, Y = get_pose_on_radius() # get_random_pose()
+                init = {'Hull.PosNED':[N,E],'Hull.PosAttitude':[0,0,Y]}
+
+                print('''\ Ep: {} - Sim: {} '''.format(ep_ix+1,sim_ix+1))
+                stime = time.time()
+                episode_reward = self.run_episode(envs[sim_ix], agents[sim_ix],**init)
+                print('''... took {:.2f} seconds, giving reward of {:.2f} '''.format(time.time() - stime, episode_reward))
+                all_rewards[sim_ix].append(episode_reward)
+
+        print('Entire training took: {} hh:mm:ss'.format(datetime.timedelta(seconds= time.time() - time_begin)))
+
+        for rewards in all_rewards:
+            plt.figure()
+            plt.plot([i for i in range(n_episodes)], rewards)
+            plt.xlabel('Episodes')
+            plt.ylabel('Episodal rewards')
+        plt.show()
+
+
+
+
+# from inside the training loop
+#     if THREADING:
+#     sim_semaphores[sim_ix].acquire()
+#     log("Locking sim" + str(sim_ix+1) + "/" + str(NUM_SIMULATORS))
+#     t = threading.Thread(target=simulate_episode, args=[sims[sim_ix]], kwargs=init) if OLD_CODE else threading.Thread(target=episode_test, args=[envs[sim_ix],agents[sim_ix]], kwargs=init)
+#     t.daemon = True
+#     t.start()
+
+#     # TODO hvis alle threads kunne ha delt en felles trajectory buffer hadde det vært awsome, slik som Spinning Up sin har!
+
+# else:
+#     if OLD_CODE:
+#         simulate_episode(sims[sim_ix],**init)

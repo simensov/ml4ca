4732319168f9c93bb0a1f2526840badd1e9009fa
simensov <simensem@gmail.com>
2020-04-06 14:40:39 +0200

    Move args to load and test for argparsing directly per script

--- src/rl/windows_workspace/load_and_test_ppo.py
+++ src/rl/windows_workspace/load_and_test_ppo.py
@@ -5,14 +5,25 @@ if __name__ == '__main__':
     from spinup.utils.test_policy import load_policy_and_env, run_policy
     from specific.trainer import Trainer
     from specific.customEnv import RevoltSimple
-    
-    from config import GLOBAL_TEST_ARGS, PPO_ARGS
 
-    _, get_action = load_policy_and_env(GLOBAL_TEST_ARGS.fpath, 
-                                        GLOBAL_TEST_ARGS.itr if GLOBAL_TEST_ARGS.itr >=0 else 'last',
-                                        GLOBAL_TEST_ARGS.deterministic)
+    import argparse
+    parser = argparse.ArgumentParser()
+    fpath = 'data\ppoReVolt\ppoReVolt_s0'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--fpath', type=str,default=fpath) # remove -- infront if wanting to use enter path from terminal as requirement
+    parser.add_argument('--len', '-l', type=int, default=0)
+    parser.add_argument('--episodes', '-n', type=int, default=5)
+    parser.add_argument('--norender', '-nr', action='store_true')
+    parser.add_argument('--itr', '-i', type=int, default=-1) # this allows for loading models from earlier epochs than the last one!
+    parser.add_argument('--deterministic', '-d', action='store_true')
+
+    args = parser.parse_args()
+
+    _, get_action = load_policy_and_env(args.fpath, 
+                                        args.itr if args.itr >=0 else 'last',
+                                        args.deterministic)
 
     t = Trainer(n_sims=1)
     t.start_simulators()
-    env = RevoltSimple(t.get_digitwins()[0],testing=True)
-    run_policy(env,get_action,max_ep_len=PPO_ARGS.steps,num_episodes=10)
+    env = RevoltSimple(t.get_digitwins()[0], testing = True, realtime = False)
+    run_policy(env,get_action,max_ep_len=env.max_ep_len, num_episodes=5)

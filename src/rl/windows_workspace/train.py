9e4f06c21355274d21f463a2441f13597007a874
simensov <simensem@gmail.com>
2020-04-08 01:28:05 +0200

    Add TRPO to the training - tested working

--- src/rl/windows_workspace/train.py
+++ src/rl/windows_workspace/train.py
@@ -6,8 +6,10 @@ Trains a ppo model with a configurable set of arguments
 @author Simen Sem Oevereng, simensem@gmail.com
 '''
 from specific.trainer import Trainer
-from spinup.algos.tf1.ppo.core import mlp_actor_critic
+from spinup.algos.tf1.ppo.core import mlp_actor_critic as ppo_ac
 from spinup.algos.tf1.ppo.ppo import ppo
+from spinup.algos.tf1.trpo.core import mlp_actor_critic as trpo_ac
+from spinup.algos.tf1.trpo.trpo import trpo
 from spinup.utils.mpi_tools import mpi_fork
 from spinup.utils.run_utils import setup_logger_kwargs
 import tensorflow as tf
@@ -27,23 +29,32 @@ if __name__ == '__main__':
     parser.add_argument('--target_kl',      type=int,   default=0.01)   # Largest KL-divergence allowed for policy network updates per minibatch.  A rough estimate of what spinning up thinks is ok is (0.01-0.05)
     parser.add_argument('--seed',           type=int,   default=0)      # Random seed
     parser.add_argument('--cpu',            type=int,   default=1)      # Number of CPU's used during training
-    parser.add_argument('--steps',          type=int,   default=1000)   # Number of steps during an entire episode for all processes combined
+    parser.add_argument('--steps',          type=int,   default=2000)   # Number of steps during an entire episode for all processes combined
     parser.add_argument('--epochs',         type=int,   default=2000)   # Number of EPISODES
     parser.add_argument('--max_ep_len',     type=int,   default=1000)   # Number of steps per local episode # (1000 is lower bound for 10 Hz steps) only affects how long each episode can be - not how many that are rolled out
     parser.add_argument('--save_freq',      type=int,   default=10)     # Number of episodes between storage of actor-critic weights
-    parser.add_argument('--exp_name',       type=str,   default='test') # Name of data storage area
+    parser.add_argument('--exp_name',       type=str,   default='newstate') # Name of data storage area
+    parser.add_argument('--algo',           type=str,   default='ppo') # Name of data storage area
+    
     args = parser.parse_args()
 
-    print('Testing implementation with {} cores'.format(args.cpu))
+    print('Training {} with {} cores'.format(args.algo.upper(), args.cpu))
     assert args.cpu == 1 or int(args.steps / args.cpu) > args.max_ep_len, 'If n_cpu > 1: The number of steps (interations between the agent and environment per epoch) per process must be larger than the largest episode to avoid empty episodal returns'
     t = Trainer(n_sims = args.cpu, start = True) 
     mpi_fork(args.cpu)  # run parallel code with mpi 
-    logger_kwargs = setup_logger_kwargs(args.exp_name, args.seed,datestamp=False)
+    logger_kwargs = setup_logger_kwargs(args.exp_name, args.seed, datestamp=False) 
     actor_critic_kwargs = {'hidden_sizes' : [args.hid]*args.l,'activation' : tf.nn.leaky_relu}
 
-    ppo(env_fn        = t.env_fn,            actor_critic  = mlp_actor_critic,
-        ac_kwargs     = actor_critic_kwargs, seed          = args.seed,     steps_per_epoch = args.steps,
-        epochs        = args.epochs,         gamma         = args.gamma,    clip_ratio      = args.clip_ratio,
-        pi_lr         = args.pi_lr,          vf_lr         = args.vf_lr,    train_pi_iters  = args.pi_epochs,
-        train_v_iters = args.vf_epochs,      lam           = args.lam,      max_ep_len      = args.max_ep_len,
-        target_kl     = args.target_kl,      logger_kwargs = logger_kwargs, save_freq       = args.save_freq)
+    if args.algo == 'ppo':
+        ppo(env_fn        = t.env_fn,            actor_critic  = ppo_ac,
+            ac_kwargs     = actor_critic_kwargs, seed          = args.seed,     steps_per_epoch = args.steps,
+            epochs        = args.epochs,         gamma         = args.gamma,    clip_ratio      = args.clip_ratio,
+            pi_lr         = args.pi_lr,          vf_lr         = args.vf_lr,    train_pi_iters  = args.pi_epochs,
+            train_v_iters = args.vf_epochs,      lam           = args.lam,      max_ep_len      = args.max_ep_len,
+            target_kl     = args.target_kl,      logger_kwargs = logger_kwargs, save_freq       = args.save_freq)
+    elif args.algo == 'trpo':
+        trpo(env_fn   = t.env_fn,            actor_critic = trpo_ac,
+            ac_kwargs = actor_critic_kwargs, seed         = args.seed,  steps_per_epoch = args.steps,
+            epochs    = args.epochs,         gamma        = args.gamma, logger_kwargs   = logger_kwargs)
+    else:
+        raise ValueError('The algorithm set is not a valid one')

db2f160b2cf1ba3287ba6aa7605b60d36d2fc0ea
simensov <simensem@gmail.com>
2020-04-14 13:49:28 +0200

    Add option of receiving NED coords from error frame, and clean the transformation

--- src/rl/windows_workspace/specific/errorFrame.py
+++ src/rl/windows_workspace/specific/errorFrame.py
@@ -11,26 +11,28 @@ class ErrorFrame(object):
             - ref   list    [north,east,heading] of the reference point (TODO could be set point to avoid dependency on reference model)
         '''
         self.update(pos,ref)
-        return
 
     def get_pose(self,new_pose=None):
         if new_pose: self.update(new_pose)
         return self._error_coordinate
 
+    def get_NED_pos(self):
+        return self._pos
+    
+    def get_NED_ref(self):
+        return self._ref
+
     def transform(self,pos=None):
         if pos: self.update(pos=pos)
 
         # TODO clean this
-        err = [a - b for a, b in zip(self._pos,self._ref)]
-        ang = wrap_angle(err[2],deg=False) # this is the same as smallest signed angle, see MSS Toolbox, ssa.m, by T.I.Fossen
-        pos_bod = ((rotation_matrix(ang).T).dot(np.array(err[0:2]).reshape(2,1))).reshape(2,).tolist() # a 2,1 vector
-
-        self._error_coordinate = [pos_bod[0], pos_bod[1], ang]
-        return 
+        err = np.array([ [a - b for a, b in zip(self._pos,self._ref)] ]).T # full shape column vector of errors
+        ang = wrap_angle(err[2,0]) # this is the same as smallest signed angle, see MSS Toolbox, ssa.m, by T.I.Fossen
+        pos_bod = rotation_matrix(ang).T.dot(err[0:2,:]) # a 2,1 column vector
+        self._error_coordinate = [pos_bod[0,0], pos_bod[1,0], ang] 
 
     def update(self,pos=None,ref=None):
         ''' Use already set values if no arguments are passed '''
         self._pos = pos if pos else self._pos
         self._ref = ref if ref else self._ref
         self.transform()
-        return

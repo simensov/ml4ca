6395c7bbdcbf2ee4e420de2b6a896f485a0e9c75
Simen Sem Øvereng <simensem@gmail.com>
2020-04-20 00:25:07 +0200

    Make args in train more compact

--- src/rl/windows_workspace/train.py
+++ src/rl/windows_workspace/train.py
@@ -18,29 +18,29 @@ import argparse
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser()
-    parser.add_argument('--hid',            type=int,   default=64)     # Number of nodes in hidden layers
-    parser.add_argument('--l',              type=int,   default=3)      # Number of hidden layers
-    parser.add_argument('--gamma',          type=float, default=0.99)   # Discount factor (0.99) NOTE from author' mujoco experience, high dim robotics works better with lower gammas, e.g. < 0.99
-    parser.add_argument('--lam',            type=float, default=0.97)   # Decay factor (0.97)
-    parser.add_argument('--clip_ratio',     type=float, default=0.2)    # Allowance for policy ratio change per update (1 +- clip_ratio)
-    parser.add_argument('--pi_lr',          type=float, default=3e-4)   # Policy network learning rate / initial step size for optimizer (3e-4)
-    parser.add_argument('--vf_lr',          type=float, default=1e-3)   # Value function network learning rate (1e-3)
-    parser.add_argument('--pi_epochs',      type=int,   default=80)     # Number of optimizer update steps on policy network per minibatch (80 originally)
-    parser.add_argument('--vf_epochs',      type=int,   default=80)     # Number of optimizer update steps on value function network per minibatch (80 originally)
-    parser.add_argument('--target_kl',      type=int,   default=0.01)   # Largest KL-divergence allowed for policy network updates per minibatch.  A rough estimate of what spinning up thinks is ok is (0.01-0.05)
-    parser.add_argument('--seed',           type=int,   default=0)      # Random seed
-    parser.add_argument('--cpu',            type=int,   default=1)      # Number of CPU's used during training
-    parser.add_argument('--steps',          type=int,   default=1600)   # Number of steps during an entire episode for all processes combined. Should be twice the size of ep_len TIMES n_cpu
-    parser.add_argument('--epochs',         type=int,   default=2000)    # Number of EPISODES
-    parser.add_argument('--max_ep_len',     type=int,   default=800)    # Number of steps per local episode # (1000 is lower bound for 10 Hz steps) only affects how long each episode can be - not how many that are rolled out
-    parser.add_argument('--save_freq',      type=int,   default=10)     # Number of episodes between storage of actor-critic weights
-    parser.add_argument('--exp_name',       type=str,   default='test') # Name of data storage area
-    parser.add_argument('--env',            type=str,   default='limited')  # Name of the algorithm used
-    parser.add_argument('--algo',           type=str,   default='ppo')  # Name of the algorithm used
-    parser.add_argument('--sim',            type=int,   default=0)      # Simulator copy used. Requires a certain number of copies of the simulator available
-    parser.add_argument('--lw',             type=bool,  default=True)  # To use the lightweight simulator or not - True can be an advantage when training for longer
-    parser.add_argument('--note',           type=str,   default='')     # Add a comment
-    parser.add_argument('--ext',            type=bool,  default=True)  # To use an extended state vector
+    parser.add_argument('--hid',        type=int,   default=64)     # Number of nodes in hidden layers
+    parser.add_argument('--l',          type=int,   default=3)      # Number of hidden layers
+    parser.add_argument('--gamma',      type=float, default=0.99)   # Discount factor (0.99) NOTE from author' mujoco experience, high dim robotics works better with lower gammas, e.g. < 0.99
+    parser.add_argument('--lam',        type=float, default=0.97)   # Decay factor (0.97)
+    parser.add_argument('--clip_ratio', type=float, default=0.2)    # Allowance for policy ratio change per update (1 +- clip_ratio)
+    parser.add_argument('--pi_lr',      type=float, default=3e-4)   # Policy network learning rate / initial step size for optimizer (3e-4)
+    parser.add_argument('--vf_lr',      type=float, default=1e-3)   # Value function network learning rate (1e-3)
+    parser.add_argument('--pi_epochs',  type=int,   default=80)     # Number of optimizer update steps on policy network per minibatch (80 originally)
+    parser.add_argument('--vf_epochs',  type=int,   default=80)     # Number of optimizer update steps on value function network per minibatch (80 originally)
+    parser.add_argument('--target_kl',  type=int,   default=0.01)   # Largest KL-divergence allowed for policy network updates per minibatch.  A rough estimate of what spinning up thinks is ok is (0.01-0.05)
+    parser.add_argument('--seed',       type=int,   default=0)      # Random seed
+    parser.add_argument('--cpu',        type=int,   default=1)      # Number of CPU's used during training
+    parser.add_argument('--steps',      type=int,   default=1600)   # Number of steps during an entire episode for all processes combined. Should be twice the size of ep_len TIMES n_cpu
+    parser.add_argument('--epochs',     type=int,   default=2000)    # Number of EPISODES
+    parser.add_argument('--max_ep_len', type=int,   default=800)    # Number of steps per local episode # (1000 is lower bound for 10 Hz steps) only affects how long each episode can be - not how many that are rolled out
+    parser.add_argument('--save_freq',  type=int,   default=10)     # Number of episodes between storage of actor-critic weights
+    parser.add_argument('--exp_name',   type=str,   default='test') # Name of data storage area
+    parser.add_argument('--env',        type=str,   default='limited')  # Name of the algorithm used
+    parser.add_argument('--algo',       type=str,   default='ppo')  # Name of the algorithm used
+    parser.add_argument('--sim',        type=int,   default=0)      # Simulator copy used. Requires a certain number of copies of the simulator available
+    parser.add_argument('--lw',         type=bool,  default=True)  # To use the lightweight simulator or not - True can be an advantage when training for longer
+    parser.add_argument('--note',       type=str,   default='')     # Add a comment
+    parser.add_argument('--ext',        type=bool,  default=True)  # To use an extended state vector
     args = parser.parse_args()
 
     print('Training {} with {} core(s)'.format(args.algo.upper(), args.cpu))

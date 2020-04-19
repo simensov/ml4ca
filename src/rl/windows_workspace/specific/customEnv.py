Improve plotting tools, test from predefined poses, correct thrust pen with torque and add a derivative penalty

src/rl/windows_workspace/specific/customEnv.py
@@ -2,7 +2,7 @@ import gym
 from gym import spaces
 import numpy as np
 import math
-from specific.misc.simtools import get_pose_3DOF, get_vel_3DOF, get_pose_on_state_space, get_pose_on_radius, get_vel_on_state_space
+from specific.misc.simtools import get_pose_3DOF, get_vel_3DOF, get_pose_on_state_space, get_random_pose_on_radius, get_vel_on_state_space, get_fixed_pose_on_radius
 from specific.misc.mathematics import gaussian, gaussian_like
 from specific.errorFrame import ErrorFrame
 
@@ -61,7 +61,7 @@ class Revolt(gym.Env):
         ''' +++++++++++++++++++++++++++++++ '''
         '''     REWARD AND TEST PARAMS      '''
         ''' +++++++++++++++++++++++++++++++ '''
-        self.vel_rew_coeffs = [0.5,0.5,0.5] # weighting between surge, sway and heading deviations used in reward function
+        self.vel_rew_coeffs = [0.5,0.5,0.5] # weighting between surge, sway and heading deviations used in reward function. Punish one rad/s twice as much as one m/s
         self.n_steps    = 1 if (testing and realtime) else 10 # I dont want to step at 100 Hz ever, really
         self.dt         = 0.01 * self.n_steps
         self.testing    = testing # stores if the environment is being used while testing policy, or is being used for training
@@ -74,6 +74,8 @@ class Revolt(gym.Env):
 
         ''' Storage of previous thrust '''
         self.prev_thrust = [0, 0, 0]
+        self.prev_angles = [0, 0, 0]
+        self.current_angles = []
 
     def __str__(self):
         return str(self.dTwin.name)
@@ -88,17 +90,20 @@ class Revolt(gym.Env):
             done (bool): whether the episode has ended, in which case further step() calls will return undefined results
             info (dict): contains auxiliary diagnostic information (helpful for debugging, and sometimes learning)
         """'''
+        self.prev_angles = self.current_angles[:]
 
         action = self.scale_and_clip(action)
         for a in self.actions:
             if a['idx'] in self.valid_action_indices:
-                # self.dTwin.val(a['module'], a['feature'], action[a['idx']]) # original, working with revoltsimple, but once the action vector couldnt be followed chronologically as for limited, it broke
                 idx = self.act_2_act_map[a['idx']]
                 self.dTwin.val(a['module'], a['feature'], action[idx])
+                if a['idx'] in [3,4,5]: # it is an angle, and in the valid action indices meaning that it has been changed
+                    self.current_angles[a['idx'] - 3] = action[idx]
 
         self.dTwin.step(self.n_steps) # ReVolt is operating at 10 Hz. Input to step() is number of steps at 100 Hz
-        s = self.state() if not self.extended_state else self.state_extended() # this uses previous time step thrust
-        self.prev_thrust = [action[0], action[1], action[2]]
+        s = self.state() if not self.extended_state else self.state_extended() # this uses previous time step thrust, so that the reward function can penalize it!
+        self.prev_thrust = [action[0], action[1], action[2]] # These three will always be the first elements of the action vector...
+
         r = self.reward()
         d = self.is_terminal()
 
@@ -118,7 +123,7 @@ class Revolt(gym.Env):
         action = np.clip(action,-bnds,bnds) # ... but the std_dev in the stochastic policy means that we have to clip
         return action.tolist()
 
-    def reset(self, new_ref = None, fraction = 0.8, **init):
+    def reset(self, new_ref = None, fraction = 0.8, fixed_point = None, **init):
         """ Resets the state of the environment and returns an initial observation.
         :returns:
             observation (object): the initial observation.
@@ -132,7 +137,10 @@ class Revolt(gym.Env):
                     N, E, Y = get_pose_on_state_space(self.real_ss_bounds[0:3], fraction = fraction)
                     u, v, r = get_vel_on_state_space(self.real_ss_bounds[3:], fraction = 0.25 * fraction)
             else: 
-                N, E, Y = get_pose_on_radius()
+                if fixed_point is None:
+                    N, E, Y = get_random_pose_on_radius()
+                else:
+                    N, E, Y = get_fixed_pose_on_radius(n = fixed_point)
 
             init = {'Hull.PosNED':[N,E],'Hull.PosAttitude':[0,0,Y], 'Hull.VelocityNu':[u,v,0,0,0,r]}
 
@@ -153,8 +161,12 @@ class Revolt(gym.Env):
             self.dTwin.val('THR'+str(i+1), 'MtcOn', 1) # turn on the motor control for all three thrusters
 
         for a in self.actions:
-            self.dTwin.val(a['module'], a['feature'], self.default_actions[a['idx']]) # set all default thruster states
+            default = self.default_actions[a['idx']]
+            self.dTwin.val(a['module'], a['feature'], default) # set all default thruster states
+            if a['idx'] in [3,4,5]: # It is an angle
+                self.prev_angles[a['idx'] - 3] = default
 
+        self.current_angles = self.prev_angles[:]
         self.prev_thrust = [0,0,0]
         s = self.state() if not self.extended_state else self.state_extended()
         return s
@@ -168,9 +180,13 @@ class Revolt(gym.Env):
 
     def is_terminal(self):
         ''' Returns true if the vessel has travelled too far from the set point.'''
+        # if self.testing: # when testing, only episode length should terminate
+        #     return False # can actually be a good indicator for bad performance though
+        
         for s, bound in zip(self.state(),self.real_ss_bounds):
             if np.abs(s) > bound:
                 return True
+
         return False
 
     def render(self):
@@ -185,21 +201,26 @@ class Revolt(gym.Env):
         :returns:
             - A float representing the scalar reward of the agent being in the current state
         '''
-        # rew = self.vel_reward() + self.smaller_yaw_dist()
-        rew = self.vel_reward() + self.multivariate_gaussian() + self.action_penalty([0.1,0.1,0.1])
-        # rew = self.vel_reward() + self.multivariate_gaussian() + self.action_penalty([0.3,0.3,0.3])
-        # rew = self.vel_reward() + self.multivariate_gaussian() + self.action_penalty([0.1,0.1,0.1], torque_based=True)
-        # rew = self.vel_reward() + self.multivariate_gaussian() + self.action_penalty([0.3,0.3,0.3], torque_based=True)
-        # rew = self.vel_reward() + self.multivariate_gaussian() + self.action_penalty([0.2,0.2,0.2])
-        # rew = self.vel_reward() + self.multivariate_gaussian() + self.action_penalty([0.2,0.2,0.2], torque_based = True)
-        # rew = self.vel_reward() + self.multivariate_gaussian() + self.action_penalty([0.2,0.2,0.2])
-        # rew = self.vel_reward() + self.multivariate_gaussian() + self.action_penalty([0.2,0.2,0.2]) + self.action_derivative_penalty([0.1,0.1,0.1])
-        # rew = self.vel_reward() + self.multivariate_gaussian() + self.action_penalty([0.1,0.1,0.1]) + self.action_derivative_penalty([0.3,0.3,0.3])
+        # rew = self.vel_reward() + self.multivariate_gaussian() + self.action_penalty([0.1,0.1,0.1]) # BEST
+        # rew = self.vel_reward() + self.multivariate_gaussian() + self.action_penalty([0.1,0.1,0.1], torque_based=True) # 0
+        # rew = self.vel_reward() + self.multivariate_gaussian() + self.action_penalty([0.3,0.3,0.3], torque_based=True) # 1
+
+        # rew = self.vel_reward() + self.multivariate_gaussian() + self.action_penalty([0.2,0.2,0.2]) + self.action_derivative_penalty([0.1,0.1,0.1]) # 2
+        # rew = self.vel_reward() + self.multivariate_gaussian() + self.action_penalty([0.1,0.1,0.1]) + self.action_derivative_penalty([0.3,0.3,0.3]) # 3
+
+        rew = self.vel_reward(coeffs=[0.5,0.5,1.0]) + self.multivariate_gaussian() + self.action_penalty([0.1,0.1,0.1]) # 4
+        rew = self.vel_reward(coeffs=[0.5,0.5,1.0]) + self.multivariate_gaussian() + self.action_penalty([0.1,0.1,0.1]) # 5
+        
+
         return rew  
 
-    def vel_reward(self):
+    def vel_reward(self, coeffs = None):
         ''' Penalizes high velocities. Maximum penalty with real_bounds and all coeffs = 1 : -2.37. All coeffs = 0.5 : -1.675'''
-        return -math.sqrt(sum( [e**2 * c for e,c in zip(get_vel_3DOF(self.dTwin), self.vel_rew_coeffs)] ))
+        if coeffs is None:
+            coeffs = self.vel_rew_coeffs
+        assert len(coeffs) == 3 and isinstance(coeffs,list), 'Vel coeffs must be a list of length 3'
+
+        return -math.sqrt(sum( [e**2 * c for e,c in zip(get_vel_3DOF(self.dTwin), coeffs)] ))
 
     def smaller_yaw_dist(self):
         ''' First reward function that fixed the steady state error in yaw by sharpening the yaw gaussian '''
@@ -229,13 +250,14 @@ class Revolt(gym.Env):
         pen = 0
         if torque_based: # The penalty is based on torque, meaning that 
             for n,c in zip(self.prev_thrust, pen_coeff):
-                pen -= np.abs(n / 100.0)**1.5 * c
+                # pen -= np.abs(n / 100.0)**1.5 * c # NBNB the power is lower than regular penalty between 0.0 and 1.0!!!
+                pen -= np.abs(n)**1.5 / 400.0 * c # This allows for the max action penalty to become 2.5 - as large as max vel penalty
         else:
             for n,c in zip(self.prev_thrust, pen_coeff):
                 pen -= np.abs(n) / 100.0 * c
         return pen # maximum penalty is 1 per time step if coeffs are <= 0.33
 
-    def action_derivative_penalty(self,pen_coeff=[0.2,0.2,0.2]):
+    def action_derivative_penalty(self,pen_coeff=[0.2,0.2,0.2], angular = False):
         if not self.extended_state:
             return 0
 
@@ -245,6 +267,13 @@ class Revolt(gym.Env):
         derr = (np.array(self.prev_thrust) - self.state_extended()[-3:]*100 )/ self.dt 
         for dT,c in zip(derr, pen_coeff):
             pen -= np.abs(dT / 100.0) * c # 200 is the maximum change from one second to another
+
+        if angular:
+            derr = (np.array(self.current_angles) - np.array(self.prev_thrust)) / self.dt
+            for dA, c in zip(derr,pen_coeff):
+                bnd = self.real_action_bounds[4] # 4 is always an angle, being the last action
+                pen - = np.abs(dA / bnd) * c # 2 * bnd is the maximum change
+
         return pen # maximum penalty is 1 per time step if coeffs are <= 0.33 and division is done by 200.0
 
 class RevoltSimple(Revolt):

src/rl/windows_workspace/specific/misc/plotters.py
@@ -65,9 +65,9 @@ def plot_NED_data(args,data):
     '''
     f1, (ax1, ax2, ax3) = plt.subplots(3, sharex=True)
     plt.xlabel('Time [s]')
-    ax1.set_ylabel('North pos')
-    ax2.set_ylabel('East pos')
-    ax3.set_ylabel('Heading [rad]')
+    ax1.set_ylabel('North pos [m]')
+    ax2.set_ylabel('East pos [m]')
+    ax3.set_ylabel('Heading [deg]')
 
     f2, ax4 = plt.subplots()
     ax4.set_xlabel('NED East')
@@ -84,12 +84,12 @@ def plot_NED_data(args,data):
             state, reference = step
             pos['north'].append(state[0]);      ref['north'].append(reference[0])
             pos['east'].append(state[1]);       ref['east'].append(reference[1])
-            pos['heading'].append(state[1]);    ref['heading'].append(reference[2])
+            pos['heading'].append(state[2]*180/np.pi );    ref['heading'].append(reference[2] * 180 / np.pi)
         
         print('Plotting ep', ep_no, 'in ned_data')
-        ax1.plot(steps,         pos['north'],   steps, ref['north'],    label='Run {}'.format(ep_no+1))
-        ax2.plot(steps,         pos['east'],    steps, ref['east'],     label='Run {}'.format(ep_no+1))
-        ax3.plot(steps,         pos['heading'], steps, ref['heading'],  label='Run {}'.format(ep_no+1))
+        ax1.plot(steps,         pos['north'],   label='Run {}'.format(ep_no+1))
+        ax2.plot(steps,         pos['east'],    label='Run {}'.format(ep_no+1))
+        ax3.plot(steps,         pos['heading'], label='Run {}'.format(ep_no+1))
         ax4.plot(pos['east'],   pos['north'],   label='Run {}'.format(ep_no+1))
         ep_no += 1
 
@@ -152,7 +152,7 @@ def plot_action_data(args,data,env):
             ax.grid(True)
             ax.legend(loc='best').set_draggable(True)
     
-    fig.tight_layout()
+    # fig.tight_layout()
 
 
 if __name__ == '__main__':

src/rl/windows_workspace/specific/misc/simtools.py
@@ -78,13 +78,31 @@ def get_random_pose():
     Y = (random.random()-0.5)*2*(math.pi)
     return N, E, Y
 
-def get_pose_on_radius(r=5, angle=5*np.pi/180):
+def get_random_pose_on_radius(r=5, angle=5*np.pi/180):
     # use polar coords to always get a position of radius r away from setpoint
     # nice for testing average rewards from each run after training, but not so nice for training due to bad exploration
     theta = random.random()*2*math.pi # random angle between origin and the place on the circle to put the vessel. NOT the same as yaw angle
     E = r * math.cos(theta)
     N = r * math.sin(theta) # y-coord -> North
     Y = random.uniform(-angle,angle)
+    return N, E, Y
+
+
+def get_fixed_pose_on_radius(n, r=5, angle=5*np.pi/180):
+    # use polar coords to always get a position of radius r away from setpoint
+    # nice for testing average rewards from each run after training, but not so nice for training due to bad exploration
+    thetas = [0.0, math.pi/4, math.pi/2, math.pi, 5*math.pi/4, 3*math.pi/2]
+    angles = [0.0,   5.0,      0.0,        5.0,       0.0,       -5.0]
+    
+    if len(thetas) <= n: # Avoid n accessing unaccesable element, warn user about it
+        print('n larger than 5 passed to fixed_points: starting on element 0')
+        n = n % len(thetas) 
+
+    NED_angle_to_unit_circle_angle = math.pi/2 - thetas[n]
+    E = r * math.cos(NED_angle_to_unit_circle_angle)
+    N = r * math.sin(NED_angle_to_unit_circle_angle) # y-coord -> North
+    Y = angles[n] * math.pi / 180
+    print('Fixed pose returns:', N, E, Y)
     return N, E, Y
 
 def get_pose_on_state_space(bounds = [5,5,np.pi/18], fraction = 1.0):
8940b5620985378b85aed9b303a44fc4bc171b69
Simen Oevereng <simen.sem.overeng@dnvgl.com>
2020-05-18 17:31:00 +0200

    Update general files in results/ root

--- results/add_prefix.py
+++ results/add_prefix.py
@@ -12,8 +12,9 @@ stern_setpoints_path = 'bagfile__{}thrusterAllocation_stern_thruster_setpoints.c
 stern_angles_path = 'bagfile__{}thrusterAllocation_pod_angle_input.csv'
 bow_params_path = 'bagfile__{}bow_control.csv'
 eta_path = path = 'bagfile__{}observer_eta_ned.csv'
+ref_path = 'bagfile__{}reference_filter_state_desired.csv'
 
-paths = [stern_setpoints_path, stern_angles_path, bow_params_path, eta_path]
+paths = [stern_setpoints_path, stern_angles_path, bow_params_path, eta_path, ref_path]
 
 assert args.p != '', 'The script demands that prefix is passed as command line argument, e.g. " python3 add_prefix.py -p "RL" "'
 
@@ -23,9 +24,8 @@ for path in paths:
 
 	try:
 		os.rename(openpath, path.format(args.p + '_'))
+		print('Added new file prefix to {}'.format(openpath))
 	except:
-		print('Could not rename', openpath)
-
-	print('Added new file prefix to {}'.format(openpath))
+		print('Could not rename', openpath, ': could it have another name or not be in the folder?')
 
 sys.exit()
